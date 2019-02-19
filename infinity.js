6c00f0f39b51b4470d40b5fcbe5f29fb1c614dc9
Alex Varghese <alex.varghese250@gmail.com>
2019-02-19 14:15:32 -0500

    infinity

---
+++ infinity.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function beyond(num){
+  if(num ===Infinity){
+    return 'And beyond';
+  }
+  else if(num>0){
+    return 'To infinity';
+  }
+  else if(num<0){
+    return 'To negative infinity';
+  }
+  else{
+    return 'Staying home';
+  }
+}
+
+console.log(beyond(100));
